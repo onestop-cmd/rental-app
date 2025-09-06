0c4d850fe4107e2de1308ade04759bb09eb87e4e
onestop-cmd <cds.sakir@gmail.com>
2025-09-06 00:23:38 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="*" element={<div>Page Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<div>Page Not Found</div>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
