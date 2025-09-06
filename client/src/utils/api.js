45e169cb4fa2c5031d23dcd40f62f591a2c659ba
onestop-cmd <cds.sakir@gmail.com>
2025-09-05 23:19:22 -0500

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,6 @@
+export const API_URL = import.meta.env.VITE_API_URL;
 
+export const getAuthHeaders = () => {
+  const token = localStorage.getItem('token');
+  return token ? { Authorization: `Bearer ${token}` } : {};
+};
