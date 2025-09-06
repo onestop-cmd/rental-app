6fa57c5da83000f674897e9701861563904c0c0a
onestop-cmd <cds.sakir@gmail.com>
2025-09-05 23:55:33 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../server/public'), // <- Build here
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
